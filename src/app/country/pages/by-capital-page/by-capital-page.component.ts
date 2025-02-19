a1b9f5fdad3e2fbfb15bfaa90e90553af670b89e
mestrada <mestrada@gmail.com>
2025-02-19 18:48:32 +0100

    Angular 18 to Angular 19

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-by-capital-page',
-  standalone: true,
-  imports: [],
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {}
--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './home-page.component.html',
+    selector: 'app-home-page',
+    imports: [RouterLink],
+    templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {}
